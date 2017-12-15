712320ab63c7517415c093251094b7269b05f6f6
dongwan123 <dongwan123@gmail.com>
2017-12-15 16:48:00 +0900

    demo success

--- src/arduino/moya_movement/moya_movement.ino
+++ src/arduino/moya_movement/moya_movement.ino
@@ -119,16 +119,16 @@ void loop() {
 void onestep(bool direction) {
     digitalWrite(dir1, direction);
     digitalWrite(stepper1, HIGH);
-    delayMicroseconds(2500);
+    delayMicroseconds(6000);
     digitalWrite(stepper1, LOW);
-    delayMicroseconds(2500);
+    delayMicroseconds(6000);
 }
 void onestep2(bool direction) {
     digitalWrite(dir2, direction);
     digitalWrite(stepper2, HIGH);
-    delayMicroseconds(2500);
+    delayMicroseconds(6000);
     digitalWrite(stepper2, LOW);
-    delayMicroseconds(2500);
+    delayMicroseconds(6000);
 }
 
 /* move functions */
