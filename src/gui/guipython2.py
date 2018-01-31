22f61069301171bb4a50c7a85f87979e376ec315
dongwan123 <dongwan123@gmail.com>
2018-01-31 12:52:05 +0900

    update gui

--- src/gui/guipython2.py
+++ src/gui/guipython2.py
@@ -12,26 +12,26 @@ LARGE_FONT= ("Verdana", 12)
 
 
 class SeaofBTCapp(tk.Tk):
-  
-    
+
+
     def __init__(self, *args, **kwargs):
 
-        
+
         tk.Tk.__init__(self, *args, **kwargs)
         tk.Tk.wm_title(self, "Test file")
 
-        
-        container = tk.Frame(self)        
+
+        container = tk.Frame(self)
         container.pack(side="top", fill="both", expand = True)
-        container.grid_rowconfigure(0, weight=1)        
+        container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        
-        for F in (StartPage, PageOne, PageTwo, PageThree, PageFour, PageRemote, PageTelephone, PageShoe, PageGlasses, PageVase, PageApple, PageTrump):               
-            frame = F(container, self)       
+
+        for F in (StartPage, PageOne, PageTwo, PageThree, PageFour, PageRemote, PageTelephone, PageShoe, PageGlasses, PageVase, PageApple, PageTrump):
+            frame = F(container, self)
             self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")   
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
@@ -61,7 +61,7 @@ class SeaofBTCapp(tk.Tk):
         receive_data()
 
     def show_frame(self, cont):
-        
+
         frame = self.frames[cont]
         frame.tkraise()
 
@@ -70,51 +70,60 @@ def qf(param):
 
 
 class StartPage(tk.Frame):
-    
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg='black')
-        
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im = Image.open("Moya_UI-01.png").resize((800,400))
-        canvas.image = ImageTk.PhotoImage(im)        
+        canvas.image = ImageTk.PhotoImage(im)
         tk.Label(canvas, image = canvas.image, bg='white').grid(row=0, column=0)
-                
+
         buttonImage = Image.open("Language.png").resize((80,80))
-        self.buttonPhoto = ImageTk.PhotoImage(buttonImage) 
+        self.buttonPhoto = ImageTk.PhotoImage(buttonImage)
         buttonImage2 = Image.open("Quiz.png").resize((80,80))
         self.buttonPhoto2 = ImageTk.PhotoImage(buttonImage2)
-        
+        buttonImage3 = Image.open('Check.png').resize((80,80))
+        self.buttonPhoto3 = ImageTk.PhotoImage(buttonImage3)
 
         button11 = tk.Button(self,image=self.buttonPhoto,compound='center',bg='white',bd = 0,
-                            command=lambda: controller.show_frame(PageOne))        
-        button11.grid(row = 380, column =775)        
+                            command=lambda: controller.show_frame(PageOne))
+        button11.grid(row = 380, column =775)
         button12 = tk.Button(self,image=self.buttonPhoto2,compound='center',bg='white',bd = 0,
                              command=lambda: controller.show_frame(PageTwo))
-        button12.grid(row = 380, column = 785)    
+        button12.grid(row = 380, column = 785)
+
+        def pub_capture():
+            pub_capture_image = rospy.Publisher('/capture_image', String, queue_size=10)
+            pub_capture_image.publish("")
+
+        button13 = tk.Button(self,image=self.buttonPhoto3,compound='center',bg='white',bd = 0,
+                            command=pub_capture)
+        button13.grid(row=380, column = 100)
 
 class PageOne(tk.Frame):
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        
+
         label = tk.Label(self, text="language", font=LARGE_FONT)
-        label.grid(row=0, column=0)    
-        
+        label.grid(row=0, column=0)
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im2 = Image.open("Moya_UI-06.png").resize((800,400))
-        canvas.image2 = ImageTk.PhotoImage(im2)        
+        canvas.image2 = ImageTk.PhotoImage(im2)
         tk.Label(canvas, image = canvas.image2, bg='white').grid(row=0, column=0)
-        
+
         buttonImage22 = Image.open("Home.png").resize((70,70))
         self.buttonPhoto22 = ImageTk.PhotoImage(buttonImage22)
-        
+
         buttonImage23 = Image.open("Save.png").resize((80,80))
         self.buttonPhoto23 = ImageTk.PhotoImage(buttonImage23)
 
         buttonImage24 = Image.open("Quiz.png").resize((80,80))
         self.buttonPhoto24 = ImageTk.PhotoImage(buttonImage24)
-        
+
         button21 = tk.Button(self,image=self.buttonPhoto22, compound='center',bg='white',bd = 0,
                             command=lambda: controller.show_frame(StartPage))
         button21.grid(row=360, column=0)
@@ -138,11 +147,11 @@ class PageOne(tk.Frame):
 
         buttonImage444 = Image.open("JAPAN.png").resize((130,130))
         self.buttonPhoto444 = ImageTk.PhotoImage(buttonImage444)
-        
+
         def pub_korean():
             pub_audio = rospy.Publisher('/language', Int16, queue_size=10)
             pub_audio.publish(1)
-        
+
         def pub_english():
             pub_audio1 = rospy.Publisher('/language', Int16, queue_size=10)
             pub_audio1.publish(11)
@@ -169,37 +178,37 @@ class PageOne(tk.Frame):
 
         button444 = tk.Button(self, image=self.buttonPhoto444, compound='center',bg='white', bd = 0,
                             command= pub_japanese)
-        button444.grid(row=250, column=490)        
+        button444.grid(row=250, column=490)
 
 
 class PageTwo(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        
+
 #        label = tk.Label(self, text="Quiz", font=LARGE_FONT)
-#        label.grid(row=0, column=0)    
-        
+#        label.grid(row=0, column=0)
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im3 = Image.open("Moya_UI-02.png").resize((800,400))
-        canvas.image3 = ImageTk.PhotoImage(im3)        
+        canvas.image3 = ImageTk.PhotoImage(im3)
         tk.Label(canvas, image = canvas.image3, bg='white').grid(row=0, column=0)
-        
+
         buttonImage = Image.open("Start.png").resize((220,220))
         self.buttonPhoto = ImageTk.PhotoImage(buttonImage)
         buttonImage22= Image.open("Home.png").resize((70,70))
         self.buttonPhoto22 = ImageTk.PhotoImage(buttonImage22)
 
-        
+
         button32 = tk.Button(self,image=self.buttonPhoto, compound='center', bg='white',
                             command=lambda: controller.show_frame(PageThree), bd = 0)
         button32.grid(row=348, column=350)
-        
+
         button33 = tk.Button(self,image=self.buttonPhoto22, compound='center', bg='white',
                             command=lambda: controller.show_frame(StartPage), bd=0)
         button33.grid(row=350, column=20)
-#        
+#
 #        button33 = ttk.Button(self, text="PageThree",
 #                            command=lambda: controller.show_frame(PageThree))
 #        button33.grid(row=370, column=775)
@@ -208,17 +217,17 @@ class PageThree(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im4 = Image.open("Moya_UI-05.png").resize((800,400))
-        canvas.image4 = ImageTk.PhotoImage(im4)        
+        canvas.image4 = ImageTk.PhotoImage(im4)
         tk.Label(canvas, image = canvas.image4, bg='white').grid(row=0, column=0)
-        
+
         im5 = Image.open("banana.jpg").resize((350,250))
-        canvas.image5 = ImageTk.PhotoImage(im5)        
+        canvas.image5 = ImageTk.PhotoImage(im5)
         label = tk.Label(self, image=canvas.image5, font=LARGE_FONT)
-        label.grid(row=360, column=450)    
+        label.grid(row=360, column=450)
 
         buttonImage24 = Image.open("Home.png").resize((70,70))
         self.buttonPhoto24 = ImageTk.PhotoImage(buttonImage24)
@@ -230,29 +239,29 @@ class PageThree(tk.Frame):
         button33 = tk.Button(self, image =  self.buttonPhoto25,compound='center', bg='white',bd = 0,
                             command=lambda: controller.show_frame(PageFour))
         button33.grid(row=370, column=770)
-        
+
 
 class PageFour(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im4 = Image.open("Moya_UI-04.png").resize((800,400))
-        canvas.image4 = ImageTk.PhotoImage(im4)        
+        canvas.image4 = ImageTk.PhotoImage(im4)
         tk.Label(canvas, image = canvas.image4, bg='white').grid(row=0, column=0)
 #        label = tk.Label(self, text="PageThree", font=LARGE_FONT)
-#        label.grid(row=100, column=400)    
-        
-        
-        
+#        label.grid(row=100, column=400)
+
+
+
         buttonImage41 = Image.open("Home.png").resize((70,70))
         self.buttonPhoto41 = ImageTk.PhotoImage(buttonImage41)
         buttonImage42 = Image.open("Next.png").resize((80,80))
         self.buttonPhoto42 = ImageTk.PhotoImage(buttonImage42)
-        
-        
+
+
         button32 = tk.Button(self, image =  self.buttonPhoto41,compound='center', bg='white',bd = 0,
                             command=lambda: controller.show_frame(StartPage))
         button32.grid(row=385, column=25)
@@ -266,11 +275,11 @@ class PageRemote(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im4 = Image.open("../object_detection/testimages/testimages/remote control/remote_005.jpg")
-        canvas.image4 = ImageTk.PhotoImage(im4)        
+        canvas.image4 = ImageTk.PhotoImage(im4)
         tk.Label(canvas, image = canvas.image4, bg='white').grid(row=0, column=0)
         # label = tk.Label(self, text="PageFive", font=LARGE_FONT)
         # label.grid(row=200, column=400)
@@ -287,11 +296,11 @@ class PageTelephone(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im4 = Image.open("AnswerTelephone.jpg")
-        canvas.image4 = ImageTk.PhotoImage(im4)        
+        canvas.image4 = ImageTk.PhotoImage(im4)
         tk.Label(canvas, image = canvas.image4, bg='white').grid(row=0, column=0)
         # label = tk.Label(self, text="PageSix", font=LARGE_FONT)
         # label.grid(row=200, column=400)
@@ -306,11 +315,11 @@ class PageShoe(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im4 = Image.open("AnswerShoes.jpg")
-        canvas.image4 = ImageTk.PhotoImage(im4)        
+        canvas.image4 = ImageTk.PhotoImage(im4)
         tk.Label(canvas, image = canvas.image4, bg='white').grid(row=0, column=0)
         # label = tk.Label(self, text="PageSix", font=LARGE_FONT)
         # label.grid(row=200, column=400)
@@ -327,11 +336,11 @@ class PageGlasses(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im4 = Image.open("AnswerGlasses.jpg")
-        canvas.image4 = ImageTk.PhotoImage(im4)        
+        canvas.image4 = ImageTk.PhotoImage(im4)
         tk.Label(canvas, image = canvas.image4, bg='white').grid(row=0, column=0)
         # label = tk.Label(self, text="PageSix", font=LARGE_FONT)
         # label.grid(row=200, column=400)
@@ -346,11 +355,11 @@ class PageVase(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im4 = Image.open("AnswerVase.jpg")
-        canvas.image4 = ImageTk.PhotoImage(im4)        
+        canvas.image4 = ImageTk.PhotoImage(im4)
         tk.Label(canvas, image = canvas.image4, bg='white').grid(row=0, column=0)
         # label = tk.Label(self, text="PageSix", font=LARGE_FONT)
         # label.grid(row=200, column=400)
@@ -365,11 +374,11 @@ class PageApple(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im4 = Image.open("AnswerApple.jpg")
-        canvas.image4 = ImageTk.PhotoImage(im4)        
+        canvas.image4 = ImageTk.PhotoImage(im4)
         tk.Label(canvas, image = canvas.image4, bg='white').grid(row=0, column=0)
         # label = tk.Label(self, text="PageSix", font=LARGE_FONT)
         # label.grid(row=200, column=400)
@@ -384,11 +393,11 @@ class PageTrump(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        
+
         canvas = tk.Canvas(self, width=800, height=400)
         canvas.grid(row=0, column=0, columnspan=800, rowspan=400)
         im4 = Image.open("AnswerTrump.jpg")
-        canvas.image4 = ImageTk.PhotoImage(im4)        
+        canvas.image4 = ImageTk.PhotoImage(im4)
         tk.Label(canvas, image = canvas.image4, bg='white').grid(row=0, column=0)
         # label = tk.Label(self, text="PageSix", font=LARGE_FONT)
         # label.grid(row=200, column=400)
@@ -405,4 +414,3 @@ if __name__=='__main__':
 
     app = SeaofBTCapp()
     app.mainloop()
-        
